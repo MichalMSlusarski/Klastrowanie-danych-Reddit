4fe70eb9eb8c3806ac230a041b1fd3ace2563bde
Michał Ślusarski <115535646+MichalMSlusarski@users.noreply.github.com>
2023-06-25 22:33:44 +0200

    Update Get-Reddit-data.py

--- Get-Reddit-data.py
+++ Get-Reddit-data.py
@@ -1,25 +1,25 @@
 import praw
 import csv
 
-# Reddit API credentials
+# Dostęp do API (ukryty)
 reddit = praw.Reddit(client_id='C9m2y3NRDX0fjzW9vGnLKw', 
                      client_secret='TZcYQwlZ1NngXLB_NmSeHNJWyV1zkw', 
                      user_agent='uni_project by /u/mars_million')
 
-# Specify the post ID
+# Numer identyfikacyjny posta
 post_id = '140xj5s'
 
-# Get the post by its ID
+# Pobranie posta
 post = reddit.submission(id=post_id)
 
-# Flatten all the comments and replies under the post
+# Wypłaszczenie, czyli zrównanie wszystkich komentarzy i odpowiedzi (dehierarchizacja)
 post.comments.replace_more(limit=0)
 comments_list = post.comments.list()
 
-# Save the comments in a CSV file
+# Utworzenie CSV i zapisanie danych
 with open(f'comments_{post.name}.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    fieldnames = ['comment', 'upvotes']
+    fieldnames = ['author', 'comment', 'upvotes']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     for comment in comments_list:
-        writer.writerow({'comment': comment.body, 'upvotes': comment.score})
+        writer.writerow({'author': comment.author, 'comment': comment.body, 'upvotes': comment.score})
